4754280e178e54d21a396c56840e69f802e211b9
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2022-07-17 23:36:53 +0200

    deleted double function in Funzone

--- src/pages/FunZone.jsx
+++ src/pages/FunZone.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
-import { GET_FUNZONE_VIDEOS } from "../GraphQL/Queries";
 import { Link } from "react-router-dom";
+import DisplayFunzone from "../components/DisplayFunzone";
 
 import "../styles/videoCard.css";
 import "../styles/Funzone.css";
-
-function DisplayFunzone() {
-  const { loading, error, data } = useQuery(GET_FUNZONE_VIDEOS);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return data.allVideos.items.map((video) => (
-    <Link key={video.id} className="videoCard" to={`/video/${video.id}`}>
-      <img
-        src={video.poster}
-        alt="posterFunzone"
-        className="imgPlaceholder"
-      ></img>
-      <h3 className="titleCard">{video.name}</h3>
-    </Link>
-  ));
-}
 
 export default function FunZone() {
   return (
